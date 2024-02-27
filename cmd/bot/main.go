83d0f8cf4dfc0ad5eb0bc36836368043f7415300
Az3lff <az3lff@gmail.com>
2024-02-27 22:29:05 +0300

    Create config file

---
+++ cmd/bot/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
+	"tg_bot_shop/internal/config"
+)
+
+func main() {
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
+		log.Printf("Failed to read config: %s", err.Error())
+	}
+
+	bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			case "withArgument":
+				msg.Text = "You supplied the following argument: " + update.Message.CommandArguments()
+			case "html":
+				msg.ParseMode = "html"
+				msg.Text = "This will be interpreted as HTML, click <a href=\"https://www.example.com\">here</a>"
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
+
+	}
+}
---
+++ internal/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	Token string
+}
+
+func LoadConfig(path string) (config *Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	if err = viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
